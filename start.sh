17e9d6d301a4ffd0e5def1614a91434b32ce0e81
Eduardo Martin Rojo <emr@sanger.ac.uk>
2017-10-16 14:54:46 +0100

    Folder to execute npm install command

--- start.sh
+++ start.sh
@@ -9,8 +9,11 @@ then
   echo "Problem: run.pid file already exists. If the script is not already running, delete the pid file."
   exit 1
 fi
+
+pushd $PROJECT_FOLDER/current
 npm install
 node $PROJECT_FOLDER/current/app.js -c $PROJECT_FOLDER/shared/nginx-selfsigned.crt -k $PROJECT_FOLDER/shared/nginx-selfsigned.key -p $PORT > $PROJECT_FOLDER/current/log.txt 2>&1 &
 
 echo $! > $PROJECT_FOLDER/current/run.pid
 echo "OK"
+popd
