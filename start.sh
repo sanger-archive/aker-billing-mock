9fa393368784f05c5d4dae54c42431d89fbc0fe1
Eduardo Martin Rojo <emr@sanger.ac.uk>
2017-10-16 13:32:26 +0100

    npm install before starting

--- start.sh
+++ start.sh
@@ -11,7 +11,7 @@ then
   echo "Problem: run.pid file already exists. If the script is not already running, delete the pid file."
   exit 1
 fi
-
+npm install
 nohup node $PROJECT_FOLDER/current/app.js -c $PROJECT_FOLDER/shared/nginx-selfsigned.crt -k $PROJECT_FOLDER/shared/nginx-selfsigned.key -p $PORT > $PROJECT_FOLDER/current/log.txt 2>&1 &
 
 echo $! > ./run.pid
