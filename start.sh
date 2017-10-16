af7aada1f1e7411be4144d45f4a55bffa919d8a1
Eduardo Martin Rojo <emr@sanger.ac.uk>
2017-10-16 14:35:12 +0100

    Scripts  to start stop

--- start.sh
+++ start.sh
@@ -1,18 +1,20 @@
 #!/bin/bash
 PROJECT_FOLDER=$1
+NODE_BINARY=$2
 PORT=3601
 
 set -e
 
-cd "`dirname $BASH_SOURCE`"
-
-if [ -f ./run.pid ]
+if [ -f $PROJECT_FOLDER/current/run.pid ]
 then
   echo "Problem: run.pid file already exists. If the script is not already running, delete the pid file."
   exit 1
 fi
-npm install
-nohup node $PROJECT_FOLDER/current/app.js -c $PROJECT_FOLDER/shared/nginx-selfsigned.crt -k $PROJECT_FOLDER/shared/nginx-selfsigned.key -p $PORT > $PROJECT_FOLDER/current/log.txt 2>&1 &
 
-echo $! > ./run.pid
+pushd $PROJECT_FOLDER/current
+npm install
+nohup $NODE_BINARY $PROJECT_FOLDER/current/app.js -c $PROJECT_FOLDER/shared/nginx-selfsigned.crt -k $PROJECT_FOLDER/shared/nginx-selfsigned.key -p $PORT > $PROJECT_FOLDER/current/log.txt 2>&1 &
+
+echo $! > $PROJECT_FOLDER/current/run.pid
 echo "OK"
+popd
