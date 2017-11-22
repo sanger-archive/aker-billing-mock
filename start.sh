e67bde5cb8ad2ac7d8b3e24e778dbacca0cc7d7d
Harriet Craven <hc6@sanger.ac.uk>
2017-11-22 09:45:56 +0000

    Changed private key and certifiate path, to match the default in ansible deployment

--- start.sh
+++ start.sh
@@ -13,7 +13,7 @@ fi
 
 pushd $PROJECT_FOLDER/current
 npm install
-nohup $NODE_BINARY $PROJECT_FOLDER/current/app.js -c $PROJECT_FOLDER/shared/nginx-selfsigned.crt -k $PROJECT_FOLDER/shared/nginx-selfsigned.key -p $PORT > $PROJECT_FOLDER/current/log.txt &
+nohup $NODE_BINARY $PROJECT_FOLDER/current/app.js -c $PROJECT_FOLDER/shared/aker-billing-facade-mock_public.crt -k $PROJECT_FOLDER/shared/aker-billing-facade-mock_private.pem -p $PORT > $PROJECT_FOLDER/current/log.txt &
 
 echo $! > $PROJECT_FOLDER/current/run.pid
 echo "OK"
